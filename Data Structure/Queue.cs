36e34a84ed4758fda158aae3d909e40f3068d9f4
“Arunesh <as8175802986@gmail.com>
2021-11-28 16:33:41 +0530

    [Arunesh]Added: code to Dequeue  Queue

---
+++ Data Structure/Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure
+{
+    class Queues
+    {
+        internal Node front;
+        internal Node rear;
+
+        public Queues()
+        {
+            this.front = null;
+            this.rear = null;
+        }
+        internal void Enqueu(int data) // To Add Data in Queues
+        {
+            Node node = new Node(data);
+            if (front == null)
+            {
+                front = node;
+                rear = node;
+            }
+            else
+            {
+                rear.next = node;
+                rear = node;
+            }
+            Console.WriteLine("Data is added  to the Queue" + data);
+        }
+        internal void Display() // To Display Data in Queues
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Node temp = front;
+                Console.Write("Element in Queue is :");
+                while (temp != null)
+                {
+                    Console.Write(temp.data + " ");
+                    temp = temp.next;
+                }
+                Console.WriteLine(" ");
+            }
+        }
+        internal void Dequeu() // To Remove Data From Queues
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Console.WriteLine("perform dequeu");
+                front = front.next;
+            }
+        }
+    }
+}
